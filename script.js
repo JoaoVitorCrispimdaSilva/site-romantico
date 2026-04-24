e590d39a048ddd9bcf5289d982ff680d151abb08
JoaoVitorCrispimdaSilva <joaovitorcrispimdasilva2@gmail.com>
2026-04-24 00:22:13 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ entrada = entrada.trim().toLowerCase();
 function entrar() {
   window.location.href = "menu.html";
 }
+let entrada = document.getElementById("senha").value;
+entrada = entrada.trim().toLowerCase().replace(/\s+/g, " ");
