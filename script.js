bafe5d3228bc9156c349b602870cb2517d17fc84
JoaoVitorCrispimdaSilva <joaovitorcrispimdasilva2@gmail.com>
2026-04-24 00:27:55 -0300

    Add files via upload

--- script.js
+++
@@ -1,6 +0,0 @@
-entrada = entrada.trim().toLowerCase();
-function entrar() {
-  window.location.href = "menu.html";
-}
-let entrada = document.getElementById("senha").value;
-entrada = entrada.trim().toLowerCase().replace(/\s+/g, " ");
